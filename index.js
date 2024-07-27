a36fdcb4ef67e8d2547a796e0f35b3b987bb26e6
udaydeep28 <udaydeepsinghd@gmail.com>
2024-07-27 23:01:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const { error } = require("console");
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+const methodOverride = require("method-override");
+
+const { v4: uuidv4 } = require("uuid");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  {
+    id: uuidv4(),
+    username: "Naruto",
+    content:
+      "If you don't like the hand that fate's dealt you with, fight for a new one.",
+  },
+  {
+    id: uuidv4(),
+    username: "Sasuke",
+    content: "It's not the future I dream of anymore, only the past.",
+  },
+  {
+    id: uuidv4(),
+    username: "Itachi",
+    content: "Those who cannot acknowledge themselves will eventually fail.",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  if (post) {
+    res.render("show.ejs", { post });
+  } else {
+    res.render("error.ejs");
+  }
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
+});
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});
