648fef1dc235ad8d0b90a9735e769c1cc1f6bab6
Udaydeep Singh Dhir <57069443+udaydeep28@users.noreply.github.com>
2024-08-21 20:07:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { error } = require("console");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require("path");
 const methodOverride = require("method-override");
 
